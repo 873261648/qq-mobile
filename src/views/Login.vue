ac2d134442286f87d91407a6fe153d9c12e58816
small white <873261648@qq.com>
2019-11-20 21:56:13 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,6 +12,7 @@
         <button :class="{'login-btn':true,'ready':qq&&password}">
             <span class="icon-right"></span>
         </button>
+        <div>注册</div>
     </div>
 </template>
 
