63ec2282da3941e54e65dfe5f1f791d2f555ea0b
small white <873261648@qq.com>
2019-12-15 20:57:41 +0800

    Update userHome.vue

--- src/views/home/other/userHome.vue
+++ src/views/home/other/userHome.vue
@@ -6,36 +6,32 @@
             <div class="right">设置</div>
         </div>
         <img class="home_bg" :src="homeBgUrl" alt="">
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-        <p>加为好友</p>
-
+        <div class="info">
+            <img :src="hisInfo.avatar" alt="">
+            <div class="text">
+                <div class="top">
+                    <p v-html="hisInfo.nickname"></p>
+                </div>
+                <div class="bottom">
+                    <p>QQ：{{hisInfo.qq}}</p>
+                    <p>
+                        <span v-html="hisInfo.gender"></span>
+                        <span v-html="hisInfo.hometown"></span>
+                        <span v-html="constellation"></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="introduction">
+            <span class="icon icon-edit-fill"></span>
+            <p v-html="hisInfo.introduction"></p>
+            <span class="icon icon-right"></span>
+        </div>
+        <div class="introduction">
+            <span class="icon icon-crown"></span>
+            <p>{{relation === 'self' ? '你':'TA'}}还未开通任何特权服务</p>
+            <span class="icon icon-right"></span>
+        </div>
 
         <div class="bottom">
             <button v-if="relation === 'stranger'">加为好友</button>
@@ -71,6 +67,23 @@
             },
             homeBgUrl () {
                 return this.hisInfo.home_bg || require('@/assets/img/defaule_home_bg.png')
+            },
+            constellation () {
+                if (!this.hisInfo.birthday) {
+                    return ''
+                }
+
+                let day = new Date(this.hisInfo.birthday).getDate()
+                let month = new Date(this.hisInfo.birthday).getMonth() + 1
+
+                let astro = ['摩羯座', '水瓶座', '双鱼座', '白羊座', '金牛座',
+                    '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座']
+                let arr = [20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22]
+                let index = month
+                if (day < arr[month - 1]) {
+                    index = index - 1
+                }
+                return astro[index]
             }
         },
         created () {
@@ -93,6 +106,9 @@
 
 <style lang="stylus" scoped>
     .user_home {
+        background-color: #fff
+        min-height 100vh
+
         .header {
             position fixed
             width 100vw
@@ -115,6 +131,7 @@
                 height 30px
                 position: absolute;
                 top 10px
+                z-index 98
 
                 &.icon-left {
                     left 10px
@@ -134,7 +151,42 @@
             object-fit cover
         }
 
-        .bottom {
+        .info {
+            display flex
+            align-items center
+            margin-top -41px
+            padding 0 20px
+
+            img {
+                width 80px
+                height 80px
+                object-fit cover
+                border-radius 80px
+                border 2px solid #fff
+                margin-right 10px
+            }
+
+            .text {
+                .top {
+                    height 42px
+                    line-height 42px
+                    color #fff
+                    font-size 18px
+                    font-weight bold
+                }
+
+                .bottom {
+                    font-size 12px
+                    line-height 21px
+
+                    span + span {
+                        margin-left 10px
+                    }
+                }
+            }
+        }
+
+        > .bottom {
             position fixed
             bottom 0
             width 100vw
